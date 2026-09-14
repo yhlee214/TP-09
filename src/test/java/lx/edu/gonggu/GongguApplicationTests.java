501419dc40baf328db42c113109cf9caac1934e3
yhlee214 <hyo2863@gmail.com>
2026-09-14 14:21:42 +0900

    init project

---
+++ src/test/java/lx/edu/gonggu/GongguApplicationTests.java
@@ -0,0 +1,13 @@
+package lx.edu.gonggu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
